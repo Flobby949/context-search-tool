e2d1ebe1e841d3a62cc14b9522a364d8efab7036
Flobby529 <2541226493@qq.com>
2026-06-14 13:19:20 +0800

    test: clean up BGE provider test scaffold

--- tests/test_embeddings_bge.py
+++ tests/test_embeddings_bge.py
@@ -1,7 +1,2 @@
-import pytest
-
-
 def test_bge_provider_can_be_imported() -> None:
     from context_search_tool.embeddings_bge import BGEEmbeddingProvider
-
-    assert BGEEmbeddingProvider is not None
